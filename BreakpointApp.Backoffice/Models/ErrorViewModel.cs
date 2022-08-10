f59d1b125d80e066cddfa20eefdebce0783ee0a7
nesherson <nesad.omeragich27@gmail.com>
2022-08-10 22:20:30 +0200

    Add project files.

---
+++ BreakpointApp.Backoffice/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BreakpointApp.Backoffice.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
