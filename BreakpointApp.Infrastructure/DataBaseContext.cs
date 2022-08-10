0d5046d6fbb1917ee9fe0fcdcca93884f0039cfe
nesherson <nesad.omeragich27@gmail.com>
2022-08-10 23:29:46 +0200

    Add migration for user

--- BreakpointApp.Infrastructure/DataBaseContext.cs
+++ BreakpointApp.Infrastructure/DataBaseContext.cs
@@ -1,7 +1,23 @@
-﻿namespace BreakpointApp.Infrastructure
-{
-    public class DataBaseContext
-    {
+﻿using BreakpointApp.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
+namespace BreakpointApp.Infrastructure
+{
+    public class DatabaseContext : DbContext
+    {
+        protected readonly IConfiguration _configuration;
+
+        public DatabaseContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql(_configuration.GetConnectionString("BreakpointApp"));
+        }
+
+        public DbSet<User> User { get; set; }
     }
 }
